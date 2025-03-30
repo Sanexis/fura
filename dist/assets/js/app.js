c645f885e53437a5e4c8737230abc48cd6dd08e7
Sanexis <amdrech1406@gmail.com>
2025-03-31 00:56:30 +0300

    feat(site): mobile scss

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -90,4 +90,21 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   };
+
+  const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+  window.addEventListener('scroll', function() {
+    if (window.pageYOffset > 300) {
+      scrollToTopBtn.style.display = 'block';
+    } else {
+      scrollToTopBtn.style.display = 'none';
+    }
+  });
+
+  scrollToTopBtn.addEventListener('click', function() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
 });
